636108d9c429e61ae59cb95f9f7df041779ad64f
Nirvashka <tixvika95@gmail.com>
2023-10-18 21:28:35 +0300

    les 01 Js практика

---
+++ Js les 01/intro.js
@@ -0,0 +1,22 @@
+let frut = prompt('Vvedit nazvy frykta:');
+console.log(frut);
+let vegetable = prompt('Vvedit nazvy ovocha:');
+console.log(vegetable);
+let frutPrice = Number(prompt('Vvedit ciny ' + frut));
+console.log(frutPrice);
+let vegetablePrice = Number(prompt('Vvedit ciny ' + vegetable));
+console.log(vegetablePrice);
+let frutQuantity = Number(prompt('Vvedit kilkit, kg ' + frut));
+console.log(frutQuantity);
+let vegetableQuantity = Number(prompt('Vvedit kilkist, kg ' + vegetable));
+console.log(vegetableQuantity);
+
+alert(frut + ' itogo ' + frutPrice * frutQuantity + ' grn!');
+alert(vegetable + ' itogo ' + vegetablePrice * vegetableQuantity + ' grn!');
+
+let takeNumber = 40 / 2;
+let surveyMenis = 47 - 6;
+let summaLater = 45 + 68;
+let showMnozch = 55 * 66;
+console.log(takeNumber, surveyMenis, summaLater, showMnozch);
+
